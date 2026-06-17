f106c0b14ea2436f64c02ae1c753bde954645604
rishi23x <subhamswn23@gmail.com>
2026-06-17 19:19:14 +0530

    Update AIOrb.jsx

--- frontend/components/AIOrb.jsx
+++ frontend/components/AIOrb.jsx
@@ -1,1 +1,23 @@
+export default function AIOrb(){
 
+return(
+
+<div className="
+h-64
+w-64
+rounded-full
+bg-gradient-to-r
+from-blue-500
+via-purple-500
+to-cyan-400
+blur-0
+animate-pulse
+shadow-2xl
+shadow-blue-500/50
+">
+
+</div>
+
+)
+
+}
