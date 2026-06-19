31328777e080e2f85d48e70ba3c0b27959ebb80e
rishi23x <subhamswn23@gmail.com>
2026-06-19 21:58:43 +0530

    Update page.jsx

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,19 +1,13 @@
-import Navbar from "../components/Navbar"
-import Hero from "../components/Hero"
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import DashboardSection from "@/components/DashboardSection";
 
-
-export default function Home(){
-
-return(
-
-<main>
-
-<Navbar/>
-
-<Hero/>
-
-</main>
-
-)
-
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <DashboardSection />
+    </main>
+  );
 }
